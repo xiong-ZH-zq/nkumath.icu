df370300ecb5106ebf8049cd4f9f7b5cb83761e4
xzqbear <68729283+xiong-ZH-zq@users.noreply.github.com>
2024-02-29 23:53:00 +0800

    Initial Commit

---
+++ table_to_md.py
@@ -0,0 +1,18 @@
+import pandas as pd  
+
+df = pd.read_excel(r'data\数统选课指南（记得屏蔽老师）.xlsx')  
+  
+for index, row in df.iterrows():
+    course_id, course_name, course_giver = row['course'].split(' ',2)
+    filename = f"docs\数院选课指南\{course_id}-{course_name}.md"  
+    course_giver = course_giver[0] + '*'
+
+    print(course_name)
+    
+    markdown_content = f"- 授课老师：{course_giver} \n\n"
+    with open(filename, 'w') as f:
+        for i in range(1,8):
+           markdown_content += f"## 评价{i}\n\n{row[f'评价{i}']}\n"  
+        f.write(markdown_content)
+  
+print("Markdown 文件已生成！")
